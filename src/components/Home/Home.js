a349ae13848f247ef24e5a108e1433c8eda554fd
Iván <ivanqgervas@gmail.com>
2021-09-28 16:01:59 +0200

    firestore conectada y funcionando

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import { collection, addDoc } from "firebase/firestore";
+
+import {db} from '../../firebase/configFirebase'
 
 class Home extends Component {
+
+
+  pruebaFirebase = async (noticia) => {
+    try {
+      const docRef = await addDoc(collection(db, "noticias"), noticia);
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+
   render() {
-    return <div>Home</div>;
+    // this.pruebaFirebase()
+    return (
+      <h1>Esto es la home</h1>
+    )
   }
 }
 
